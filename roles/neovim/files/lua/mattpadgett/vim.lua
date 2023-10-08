53f1a3b14e83780421c6dd0289abffa69b51af5a
Matt Padgett <mattpadgett@outlook.com>
2023-10-08 16:16:23 -0500

    adding all the new dotfiles

---
+++ roles/neovim/files/lua/mattpadgett/vim.lua
@@ -0,0 +1,55 @@
+local function set_vim_g()
+  vim.g.mapleader = " "
+end
+
+local function set_vim_o()
+  local settings = {
+    backup = false,
+    errorbells = false,
+    expandtab = true,
+    hidden = true,
+    scrolloff = 3,
+    softtabstop = 2,
+    showmode = false,
+    termguicolors = true
+  }
+
+  -- Generic vim.o
+  for k, v in pairs(settings) do
+    vim.o[k] = v
+  end
+
+  -- Custom vim.o
+  vim.o.clipboard = 'unnamedplus'
+  vim.o.shortmess = vim.o.shortmess .. 'c'
+
+  -- Not yet in vim.o
+  vim.cmd('set encoding=utf8')
+  vim.cmd('set nowritebackup')
+  vim.cmd('set shiftwidth=2')
+  vim.cmd('set secure')
+  vim.cmd('set splitright')
+  vim.cmd('set tabstop=2')
+  vim.cmd('set updatetime=300')
+end
+
+local function set_vim_wo()
+  vim.wo.number = true
+  vim.wo.relativenumber = true
+  vim.wo.wrap = false
+end
+
+local function set_color()
+  vim.cmd.colorscheme "catppuccin-macchiato"
+end
+
+local function init()
+  set_vim_g()
+  set_vim_o()
+  set_vim_wo()
+  set_color()
+end
+
+return {
+  init = init
+}
---
+++ roles/neovim/files/plugin/general.lua
@@ -0,0 +1,4 @@
+local map = vim.api.nvim_set_keymap
+local set = vim.keymap.set
+
+map("i", "qq", "<ESC>", { noremap = true })
